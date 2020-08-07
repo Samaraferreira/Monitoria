9a3552206845b7d483aa3d9e305c5fba7bcdd3d9
Samara Ferreira <ferreirasamara.sf@gmail.com>
2020-08-07 11:51:33 -0300

    feat: add route to create teacher

---
+++ server/src/routes.ts
@@ -0,0 +1,15 @@
+import express from 'express'
+import multer from 'multer'
+import uploadConfig from '@config/upload'
+
+import TeacherController from './controllers/TeacherController'
+
+const routes = express.Router()
+const upload = multer(uploadConfig)
+
+const teacherController = new TeacherController()
+
+// routes.get('/teacher', teacherController.index)
+routes.post('/teachers', upload.single('avatar'), teacherController.create)
+
+export default routes
