b9c850ba24ff86b90be0a23ef9638592fa343e08
Samara Ferreira <ferreirasamara.sf@gmail.com>
2020-08-07 23:39:38 -0300

    feat: add link to go back

--- web/src/components/Header/index.tsx
+++ web/src/components/Header/index.tsx
@@ -8,9 +8,10 @@ import './styles.css';
 
 interface HeaderProps {
   title: string;
+  description?: string;
 }
 
-const Header: React.FC<HeaderProps> = ({ title, children }) => {
+const Header: React.FC<HeaderProps> = ({ title, description, children }) => {
   return (
     <header className="page-header">
       <div className="top-bar-container">
@@ -23,6 +24,8 @@ const Header: React.FC<HeaderProps> = ({ title, children }) => {
 
       <div className="header-content">
         <strong>{title}</strong>
+        {description && <p>{description}</p>}
+
         {children}
       </div>
     </header>
