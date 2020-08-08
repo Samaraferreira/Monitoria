965a2e5524a14c6858edbe43616fa92dca78d295
Samara Ferreira <ferreirasamara.sf@gmail.com>
2020-08-07 23:46:17 -0300

    feat: create TeacherForm route

--- web/src/routes.tsx
+++ web/src/routes.tsx
@@ -3,13 +3,15 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 import Landing from './pages/Landing';
 import List from './pages/List';
+import TeacherForm from './pages/TeacherForm';
 
 const Routes: React.FC = () => {
   return (
     <BrowserRouter>
       <Switch>
         <Route path="/" exact component={Landing} />
-        <Route path="/monitores" component={List} />
+        <Route path="/study" component={List} />
+        <Route path="/give-classes" component={TeacherForm} />
       </Switch>
     </BrowserRouter>
   );
