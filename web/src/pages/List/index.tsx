a362cecb1599aac658ed97baa12ff83ed75b9de5
Samara Ferreira <ferreirasamara.sf@gmail.com>
2020-08-07 23:33:39 -0300

    feat: connect api

--- web/src/pages/List/index.tsx
+++ web/src/pages/List/index.tsx
@@ -1,29 +1,31 @@
-import React from 'react';
+import React, { useState, FormEvent } from 'react';
 
 import Header from '../../components/Header';
 import TeacherItem from '../../components/TeacherItem';
+import Select from '../../components/Select';
+
+import subjects from '../../assets/data/subjects.json';
+
+import api from '../../services/api';
 
 import './styles.css';
 
+
 const List: React.FC = () => {
+  const [subject, setSubject] = useState('');
+
   return (
     <div id="page-teacher-list" className="container">
       <Header title="Esses são os monitores disponíveis.">
         <form id="search-teachers">
-          <div className="input-block">
-            <label htmlFor="subject">Matéria</label>
-            <input type="text" id="subject" />
-          </div>
-
-          <div className="input-block">
-            <label htmlFor="week_day">Dia da semana</label>
-            <input type="text" id="week_day" />
-          </div>
-
-          <div className="input-block">
-            <label htmlFor="time">Hora</label>
-            <input type="text" id="time" />
-          </div>
+          <Select
+            name="subject"
+            label="Matéria"
+            options={subjects}
+            value={subject}
+            onChange={(e) => setSubject(e.target.value)}
+          />
+          <button type="submit">Buscar</button>
         </form>
       </Header>
 
