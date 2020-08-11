9264cba2b8ce7e27d56e8c5ecb1b64d3b736d554
Samara Ferreira <ferreirasamara.sf@gmail.com>
2020-08-11 01:20:35 -0300

    feat: create Success page

---
+++ web/src/pages/Success/index.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import successIcon from '../../assets/images/icons/success-check-icon.svg';
+
+import './styles.css';
+
+const Success: React.FC = () => {
+  return (
+    <div id="page-success">
+      <img src={successIcon} alt="Sucesso" />
+      <h1>Cadastro salvo!</h1>
+      <p>Tudo certo, seu cadastro está na nossa lista de monitores.</p>
+      <Link to="/study">Acessar lista</Link>
+    </div>
+  );
+};
+
+export default Success;
